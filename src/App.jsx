8a98df6fc1bd880ae0c9d085e9c09d244f8d74ac
vikash5438 <82951929+vikash5438@users.noreply.github.com>
2025-03-12 23:39:44 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ProductPage from "./components/ProductPage";
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8">
+      <ProductPage></ProductPage>
+    </div>
+  );
+}
+
+export default App;
